1978c13bf26cc03746b565c0e09d17f3155c0e74
XeanP <101090719+XeanP@users.noreply.github.com>
2025-06-05 16:14:03 -0500

    creacion de capas

---
+++ CapaAccesoDatos/Conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaAccesoDatos
+{
+    public class Conexion
+    {
+        //patron de Diseño Singleton
+        private static readonly Conexion _instancia = new Conexion();
+        public static Conexion Instancia
+        {
+            get { return Conexion._instancia; }
+        }
+        public SqlConnection Conectar()
+
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=DESKTOP-62V8LFN;Initial Catalog=PedregalDB;Integrated Security=true";
+            return cn;
+           
+        }
+
+    }
+}
