d31ef543fdecd219f011911b132b3b4c1807230f
angheloar <angheloarroyomera@gmail.com>
2025-06-24 07:04:45 -0500

    MantenedorDisponibilidadEmpleado

---
+++ PedregalApp/MantenedorDisponibilidadPersonal.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace PedregalApp
+{
+    partial class MantenedorDisponibilidadPersonal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.grupBoxDatos = new System.Windows.Forms.GroupBox();
+            this.txtCodEmpleado = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dtPickerRegBin = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRealizaConsulta = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dgvBin = new System.Windows.Forms.DataGridView();
+            this.grupBoxDatos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(659, 37);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(129, 42);
+            this.btnNuevo.TabIndex = 2;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(659, 102);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(129, 42);
+            this.btnModificar.TabIndex = 3;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(659, 164);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(129, 42);
+            this.btnConsultar.TabIndex = 4;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // grupBoxDatos
+            // 
+            this.grupBoxDatos.Controls.Add(this.dateTimePicker1);
+            this.grupBoxDatos.Controls.Add(this.txtCodEmpleado);
+            this.grupBoxDatos.Controls.Add(this.label3);
+            this.grupBoxDatos.Controls.Add(this.btnCancelar);
+            this.grupBoxDatos.Controls.Add(this.button1);
+            this.grupBoxDatos.Controls.Add(this.btnAgregar);
+            this.grupBoxDatos.Controls.Add(this.dtPickerRegBin);
+            this.grupBoxDatos.Controls.Add(this.label2);
+            this.grupBoxDatos.Controls.Add(this.label1);
+            this.grupBoxDatos.Controls.Add(this.btnRealizaConsulta);
+            this.grupBoxDatos.Location = new System.Drawing.Point(12, 271);
+            this.grupBoxDatos.Name = "grupBoxDatos";
+            this.grupBoxDatos.Size = new System.Drawing.Size(615, 158);
+            this.grupBoxDatos.TabIndex = 6;
+            this.grupBoxDatos.TabStop = false;
+            this.grupBoxDatos.Text = "Disponibilidad personal";
+            // 
+            // txtCodEmpleado
+            // 
+            this.txtCodEmpleado.Location = new System.Drawing.Point(120, 28);
+            this.txtCodEmpleado.Name = "txtCodEmpleado";
+            this.txtCodEmpleado.Size = new System.Drawing.Size(100, 20);
+            this.txtCodEmpleado.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Codigo empleado";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(444, 102);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(152, 31);
+            this.btnCancelar.TabIndex = 7;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(444, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Modificar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(444, 25);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(152, 31);
+            this.btnAgregar.TabIndex = 5;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // dtPickerRegBin
+            // 
+            this.dtPickerRegBin.Location = new System.Drawing.Point(120, 58);
+            this.dtPickerRegBin.Name = "dtPickerRegBin";
+            this.dtPickerRegBin.Size = new System.Drawing.Size(200, 20);
+            this.dtPickerRegBin.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fecha fin";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fecha inicio";
+            // 
+            // btnRealizaConsulta
+            // 
+            this.btnRealizaConsulta.Location = new System.Drawing.Point(444, 25);
+            this.btnRealizaConsulta.Name = "btnRealizaConsulta";
+            this.btnRealizaConsulta.Size = new System.Drawing.Size(152, 31);
+            this.btnRealizaConsulta.TabIndex = 11;
+            this.btnRealizaConsulta.Text = "Realizar consulta";
+            this.btnRealizaConsulta.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(120, 94);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // dgvBin
+            // 
+            this.dgvBin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBin.Location = new System.Drawing.Point(23, 26);
+            this.dgvBin.Name = "dgvBin";
+            this.dgvBin.ReadOnly = true;
+            this.dgvBin.Size = new System.Drawing.Size(615, 213);
+            this.dgvBin.TabIndex = 7;
+            // 
+            // MantenedorDisponibilidadPersonal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvBin);
+            this.Controls.Add(this.grupBoxDatos);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnNuevo);
+            this.Name = "MantenedorDisponibilidadPersonal";
+            this.Text = "MantenedorDisponibilidadPersonal";
+            this.grupBoxDatos.ResumeLayout(false);
+            this.grupBoxDatos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBin)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.GroupBox grupBoxDatos;
+        private System.Windows.Forms.TextBox txtCodEmpleado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DateTimePicker dtPickerRegBin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRealizaConsulta;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dgvBin;
+    }
+}
